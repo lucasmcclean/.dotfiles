be35f88f176fe93fcca963d9b19de4b6279a117b
Lucas McClean <lucksdeveloper@gmail.com>
2025-06-16 22:29:39 +0200

    feat(nvim): transparent background

--- nvim/.config/nvim/lua/plugins/theme.lua
+++ nvim/.config/nvim/lua/plugins/theme.lua
@@ -1,11 +1,21 @@
 return {
-  "navarasu/onedark.nvim",
-  lazy = false,
-  priority = 1000,
-  config = function()
-    require("onedark").setup({
-      style = "darker"
-    })
-    vim.cmd.colorscheme("onedark")
-  end,
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("tokyonight").setup({
+        transparent = true,
+        styles = {
+          sidebars = "transparent",
+          floats = "transparent",
+        },
+      })
+      vim.cmd.colorscheme("tokyonight")
+      vim.api.nvim_set_hl(0, "StatusLine", { bg = "none", fg = "#a9b1d6" })
+      vim.api.nvim_set_hl(0, "StatusLineNC", { bg = "none", fg = "#5c6370" })
+      vim.api.nvim_set_hl(0, "TabLineSel", { bg = "none" })
+      vim.api.nvim_set_hl(0, "MsgArea", { bg = "none" })
+    end,
+  },
 }
