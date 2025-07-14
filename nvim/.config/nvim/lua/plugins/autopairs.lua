7c4b97074e07e69987ae43570fee3c03c9372a7c
Lucas McClean <lucksdeveloper@gmail.com>
2025-07-14 10:56:30 +0200

    feat(nvim): add autotags for html

--- nvim/.config/nvim/lua/plugins/autopairs.lua
+++ nvim/.config/nvim/lua/plugins/autopairs.lua
@@ -6,4 +6,14 @@ return {
       require("nvim-autopairs").setup({})
     end,
   },
+  {
+    "windwp/nvim-ts-autotag",
+    lazy = false,
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+    },
+    config = function()
+      require("nvim-ts-autotag").setup()
+    end,
+  },
 }
