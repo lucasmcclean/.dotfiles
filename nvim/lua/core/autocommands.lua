fa78fbb4c3feadb77a5d28c28c2dd0da01ab1274
Lucas McClean <lucksdeveloper@gmail.com>
2026-01-08 13:46:20 +0000

    feat(nvim): display word count
    only in txt-like files

--- nvim/lua/core/autocommands.lua
+++ nvim/lua/core/autocommands.lua
@@ -28,3 +28,27 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.opt_local.formatoptions:append("t")
   end,
 })
+
+local function wc_rt()
+  local word_count = vim.fn.wordcount().words
+  local reading_time = word_count / 200
+  return string.format("%d:%0.1f", word_count, reading_time)
+end
+
+_G.wc_rt = wc_rt
+
+local function is_text_file()
+  local ext = vim.fn.expand('%:e')
+  return ext == 'txt' or ext == 'md'
+end
+
+vim.api.nvim_create_autocmd("BufEnter", {
+  pattern = "*",
+  callback = function()
+    if is_text_file() then
+      vim.opt.statusline = "  %f %m %= %{v:lua.wc_rt()}  %l,%c  %p%% "
+    else
+      vim.opt.statusline = "  %f %m %= %l,%c  %p%%  "
+    end
+  end,
+})
