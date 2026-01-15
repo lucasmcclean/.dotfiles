871aebe6626a346f9ac8deb61c00822b1a828935
Lucas McClean <lucksdeveloper@gmail.com>
2026-01-15 22:00:02 +0000

    feat(bash): add aliases, pyenv, sdkman

--- bash/.bashrc.d/aliases_general.sh
+++ bash/.bashrc.d/aliases_general.sh
@@ -1,7 +1,11 @@
 alias c="clear"
+alias zzz="sudo zzz"
+alias at='cat /sys/class/power_supply/BAT0/capacity'
+
+alias eustis="ssh lu170416@eustis.eecs.ucf.edu"
+
 alias ..='cd ..'
 alias ...='cd ../..'
 alias ....='cd ../../..'
 alias .....='cd ../../../..'
 alias ......='cd ../../../../..'
-alias at='cat /sys/class/power_supply/BAT0/capacity'
