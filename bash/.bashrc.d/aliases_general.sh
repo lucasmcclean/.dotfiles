3e2f5dc517459a80be72f38e8bbb7443a87c25f6
Lucas McClean <lucksdeveloper@gmail.com>
2026-01-20 12:01:08 +0000

    feat(bash): add verbose eustis

--- bash/.bashrc.d/aliases_general.sh
+++ bash/.bashrc.d/aliases_general.sh
@@ -3,6 +3,7 @@ alias zzz="sudo zzz"
 alias at='cat /sys/class/power_supply/BAT0/capacity'
 
 alias eustis="ssh lu170416@eustis.eecs.ucf.edu"
+alias eustisv="ssh -v lu170416@eustis.eecs.ucf.edu"
 
 alias ..='cd ..'
 alias ...='cd ../..'
