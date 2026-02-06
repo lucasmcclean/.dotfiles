2cb86dc6a7e4792dbd167d148ebfb50185873e73
Lucas McClean <lucksdeveloper@gmail.com>
2026-02-06 16:19:40 +0000

    feat(bash): 'at' now includes battery status

--- bash/.bashrc.d/aliases_general.sh
+++ bash/.bashrc.d/aliases_general.sh
@@ -1,6 +1,10 @@
 alias c="clear"
 alias zzz="sudo zzz"
-alias at='cat /sys/class/power_supply/BAT0/capacity'
+alias at='
+  cat \
+  /sys/class/power_supply/BAT0/capacity \
+  /sys/class/power_supply/BAT0/status
+'
 
 alias eustis="ssh lu170416@eustis.eecs.ucf.edu"
 alias eustisv="ssh -v lu170416@eustis.eecs.ucf.edu"
