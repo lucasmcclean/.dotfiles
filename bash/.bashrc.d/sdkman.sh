e3074397ecc6e927f91ab0875345d8fb9ea9c666
Lucas McClean <lucksdeveloper@gmail.com>
2026-03-14 18:49:29 -0400

    fix(bash): correct sdkman init

--- bash/.bashrc.d/sdkman.sh
+++ bash/.bashrc.d/sdkman.sh
@@ -1,6 +1,6 @@
-if [ -s "$SDKMAN_DIR/bin/sdkman-init.sh" ]; then
-    export SDKMAN_DIR="$HOME/.sdkman"
+export SDKMAN_DIR="$HOME/.sdkman"
 
+if [ -s "$SDKMAN_DIR/bin/sdkman-init.sh" ]; then
     . "$SDKMAN_DIR/bin/sdkman-init.sh"
 
     if [ -d "$SDKMAN_DIR/candidates/java/21-tem" ]; then
